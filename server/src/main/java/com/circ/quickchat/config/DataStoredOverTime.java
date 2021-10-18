0ca7b2eddb28986002c16e42ec6003366c3a8510
RCL98 <48773399+RCL98@users.noreply.github.com>
2021-10-18 20:39:46 +0300

    fix websocket connection 2

--- server/src/main/java/com/circ/quickchat/config/DataStoredOverTime.java
+++ server/src/main/java/com/circ/quickchat/config/DataStoredOverTime.java
@@ -2,6 +2,10 @@ package com.circ.quickchat.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import constant.ChatConstants;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +23,9 @@ public class DataStoredOverTime {
 	
 	@Bean
 	public Map<String, Chat> chats() {
-		return new HashMap<String, Chat>();
+		HashMap<String, Chat> chats = new HashMap<String, Chat>();
+		chats.put(ChatConstants.principalChatId, Chat.builder().id(UUID.randomUUID().toString()).build());
+		return chats;
 	}
 
 }
--- ui/src/app/constants.js
+++ ui/src/app/constants.js
@@ -3,5 +3,6 @@ export const UPGRADE_LIST_USERS = "UPGRADE_LIST_USERS";
 export const ADD_USER = "ADD_USER";
 export const DELETE_USER = "DELETE_USER";
 export const UPDATE_USER = "UPDATE_USER";
+export const CHAT_MESSAGE = "CHAT_MESSAGE";
 
 export const serverHost = process.env.NODE_ENV !== "development" ? "" : "http://localhost:8080";
